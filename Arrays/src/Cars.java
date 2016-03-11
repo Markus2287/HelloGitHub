052adfe1b054b795c89a75b7fd3edc21dc04b44e
Markus2287 <XMG@172.31.36.161>
2016-03-11 19:58:58 +0100

    First Commit

---
+++ Arrays/src/Cars.java
@@ -0,0 +1,28 @@
+
+public class Cars implements Comparable{
+	
+	private double price;
+	
+	@Override
+	public int compareTo(Object arg0) {
+		return 0;
+	}
+	
+	
+	
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	
+	
+		// TODO Auto-generated method stub
+
+	}
+
+
---
+++ Arrays/src/CarsDemo.java
@@ -0,0 +1,10 @@
+
+public class CarsDemo {
+
+	 public static void main (String[] args){
+		 Car[] cars = {new Car(20000), new Car (10000)};
+			Arrays.sort(cars);
+	 }
+	
+	
+}
